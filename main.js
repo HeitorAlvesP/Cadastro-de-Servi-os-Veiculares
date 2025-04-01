9be5880a5d4a91e1380ab3d177193bb343cf6270
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2025-04-01 09:48:49 -0300

    mensagens de comfirmação

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
 document.getElementById("confirmar").addEventListener("click", function () {
+    const nome = document.getElementById("nome").value.trim();
+    const senha = document.getElementById("senha").value.trim();
+    const modelo = document.getElementById("modelo").value;
+    const observacoes = document.getElementById("observacoes").value.trim();
+    const servicos = document.querySelectorAll('input[name="servico"]:checked');
+    const veiculo = document.querySelector('input[name="tipo-veiculo"]:checked');
+
+    if (!nome || !senha || !observacoes || !modelo  || servicos.length === 0 || !veiculo) {
+        Swal.fire({
+            icon: "info",
+            title: "AVISO",
+            text: "Preencha todos os campos antes de agendar.",
+            confirmButtonText: "OK"
+        });
+        return;
+    }
+
     Swal.fire({
         icon: "success",
-        title: "Confirmado",
+        title: "Agendamento Confirmado",
         text: "Seu agendamento foi realizado com sucesso!",
         confirmButtonText: "OK"
     });
 });
 
-
 document.getElementById("cancelar").addEventListener("click", function () {
     Swal.fire({
         icon: "error",
-        title: "Cancelado",
+        title: "Cancelamento",
         text: "O agendamento foi cancelado!",
         confirmButtonText: "OK"
     });
+
     document.getElementById("formAgendamento").reset();
 });
