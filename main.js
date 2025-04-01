a3f1999d256425c67c16347b64bc5ce1977565e9
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2025-04-01 09:25:15 -0300

    clean

--- main.js
+++ main.js
@@ -12,26 +12,6 @@ document.getElementById("cancelar").addEventListener("click", function () {
     Swal.fire({
         icon: "error",
         title: "Cancelado",
-        text: "Seu agendamento foi cancelado.",
-        confirmButtonText: "OK"
-    });
-});
-
-
-document.getElementById("confirmar").addEventListener("click", function () {
-    Swal.fire({
-        icon: "success",
-        title: "Agendamento Confirmado",
-        text: "Seu agendamento foi realizado com sucesso!",
-        confirmButtonText: "OK"
-    });
-});
-
-
-document.getElementById("cancelar").addEventListener("click", function () {
-    Swal.fire({
-        icon: "error",
-        title: "Cancelamento",
         text: "O agendamento foi cancelado!",
         confirmButtonText: "OK"
     });
